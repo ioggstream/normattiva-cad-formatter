f3920e525c9126fc9a815d36eb79e543c775c937
Roberto Polli <robipolli@gmail.com>
2021-01-15 17:58:49 +0100

    improve parsing

--- tests/test_capo.py
+++ tests/test_capo.py
@@ -24,6 +24,29 @@ Decreto Legislativo 7 marzo 2005, n. 82
 """
 
 
+def fix_accent(l):
+    """
+    Sostituisce apostrofo con l'accento in parole che finiscono per vocale.
+    Es: liberta' -> libertà
+    Po', po', è vengono ripristinati nella versione corretta
+    """
+    l = l.replace("a'", "à").replace("e'", "é").replace("i'", "ì")
+    l = l.replace("o'", "ò").replace("u'", "ù").replace("E'", "È")
+    l = (
+        l.replace("pò", "po'")
+        .replace("Pò", "Po'")
+        .replace(" é ", " è ")
+        .replace("e\\'", "è")
+        .replace("cioé", "cioè")
+    )
+    if l[-2:] == " é":
+        l = l[:-1] + "è"
+    if l[0:2] == "é ":
+        l = "è" + l[1:]
+
+    return l
+
+
 def parse_articolo(a):
     """
 <articolo id="19">
@@ -41,9 +64,21 @@ def parse_articolo(a):
 
     """
     lines = a.xpath(".//corpo/p/text()")
-    title, *body = [x.extract() for x in lines]
+    lines = [x.extract() for x in lines]
+    lines = [re.sub(r"^\(\(|\)\)$", "", l) for l in lines]
+    lines = [re.sub(r"^([a-z\-]+)\) ", r"\n  \1\) ", l) for l in lines]
+    lines = [re.sub(r"^([0-9a-z\-]+)\. ", r"\n  \1. ", l) for l in lines]
+
+    for i, l in enumerate(lines):
+        if l.startswith("Art"):
+            break
+    intro = lines[:i]
+    art, headline, *body = lines[i:]
+    title = art + ". " + headline
     txt_lines = [title, "^" * len(title), ""] + body + ["\n"]
-    return "\n".join(txt_lines)
+    txt_intro = fix_accent("\n".join(intro + ["\n"])) if i else None
+    txt_lines = fix_accent("\n".join(txt_lines))
+    return txt_intro, txt_lines
 
 
 import re
@@ -64,6 +99,14 @@ def mkfilename(capo_id, sezione_id=None, art_id=None):
     )
 
 
+def get_capo_titolo(text):
+    if "Sezione" in text:
+        capo_titolo, *_ = re.findall("(Capo .*)(Sezione.*)", text, re.I)[0]
+        return capo_titolo
+
+    return re.findall("(capo .*)", text, re.I)[0]
+
+
 class CAD(object):
     def __init__(self, text):
         self.cad = Selector(text=text)
@@ -75,19 +118,23 @@ class CAD(object):
             sezione, text = parse_capo(s)
             if text.startswith("Capo"):
                 capo = re.findall("Capo ([A-Z0-9]+)", text)[0]
-                self.capi[capo] = {"titolo": text, "sezioni": []}
+                capo_titolo = get_capo_titolo(text)
+                self.capi[capo] = {"titolo": capo_titolo, "sezioni": []}
+
             sezione = re.findall("Sezione ([A-Z0-9]+)", text)
             sezione = "" if not sezione else sezione[0]
             sezione_titolo = re.findall("(Sezione .*)", text)
             print(capo, sezione, f"[{text}]")
             sezione_o = {
-                sezione: sezione_titolo[0] if sezione_titolo else "",
+                sezione: fix_accent(sezione_titolo[0] if sezione_titolo else ""),
                 "articoli": [],
+                "intro": "foo",
             }
             for a in s.xpath(".//articolo"):
-                txt = parse_articolo(a)
-                sezione_o["articoli"].append(txt)
-
+                intro_txt, articolo_txt = parse_articolo(a)
+                sezione_o["articoli"].append(articolo_txt)
+                if intro_txt:
+                    sezione_o["intro"] = intro_txt
             self.capi[capo]["sezioni"].append(sezione_o)
 
     def dump_index(self):
@@ -101,20 +148,26 @@ class CAD(object):
         dpath = Path("dist")
         for capo_id, capo in self.capi.items():
             capo_fpath = dpath / f"capo_{capo_id}.rst"
-            capo_titolo = capo["titolo"]
-            capo_txt = [f"{capo_titolo}", "=" * len(capo_titolo), "\n.. toctree::\n"]
+            capo_titolo = fix_accent(capo["titolo"])
+            capo_txt = [
+                f"{capo_titolo}",
+                "=" * (len(capo_titolo) + 2),
+                "\n.. toctree::\n",
+            ]
 
             for sezione in capo["sezioni"]:
                 sezione_id = next(iter(sezione.keys()))
                 sezione_titolo = sezione[sezione_id]
-
+                sezione_intro = sezione.get("intro", "")
                 if sezione_id:
                     sezione_fpath = dpath / mkfilename(capo_id, sezione_id)
                     capo_txt += [f"   {str(sezione_fpath).replace('dist/','')}"]
                     sezione_txt = [
                         sezione_titolo,
-                        "-" * len(sezione_titolo),
-                        "\n.. toctree::\n",
+                        "-" * (len(sezione_titolo) + 2),
+                        "",
+                        sezione_intro,
+                        "" "\n.. toctree::\n",
                     ]
                     art_dest = sezione_txt
                 else:
@@ -135,7 +188,9 @@ class CAD(object):
 def parse_capo(e):
     text = "".join(e.xpath("node()/text()").extract()).strip().strip(" -").strip("(")
     sezione = e.xpath("@id").extract()[0]
-    return sezione, text
+    sezione = re.sub("[\r\n]", " ", sezione)
+    text = re.sub("[\r\n]", " ", text)
+    return sezione, fix_accent(text)
 
 
 def test_cad(cad):
